65bb757223a3b6b506fb40494339ce9f9fc96986
Emilio Luna Pérez <al244182@edu.uaa.mx>
2023-01-30 23:22:01 -0600

    Changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 
+    projectId: 'emilioenlaluna-blog',
+    appId: '1:416212324735:web:b0af5a1adacfff36c30a4a',
+    storageBucket: 'emilioenlaluna-blog.appspot.com',
+    apiKey: 'AIzaSyD7OulWh9Hr9jTkma_AKhoO19zsAUkI84E',
+    authDomain: 'emilioenlaluna-blog.firebaseapp.com',
+    messagingSenderId: '416212324735',
+  },
+    production: false,
+  };
